8f2aa74468bf02908a0502b26f72025e5eedb745
Choor Hong <abner.hong@hotmail.com>
2021-06-28 23:33:12 +0800

    Add grahql resolvers

--- backend/graphql/resolvers.ts
+++ backend/graphql/resolvers.ts
@@ -4,54 +4,53 @@ import mongoose from 'mongoose'
 import Task from '../models/todo'
 
 interface IInputTask {
-    input: {
-        id?: string;
-        text: string;
-    }
+  input: {
+    id?: string
+    text: string
+  }
 }
 
-
 const graphqlResolvers = {
-    getSimpleTest: () => 'Hello World',
+  getSimpleTest: () => 'Hello World',
 
-    getTasks: async () => {
-        const tasks = await Task.find({})
-        return tasks
-    },
+  getTasks: async () => {
+    const tasks = await Task.find({})
+    return tasks
+  },
 
-    postTask: async (args: IInputTask, req: Request) => {
-        const { input } = args
+  postTask: async (args: IInputTask, req: Request) => {
+    const { input } = args
 
-        const content = new Task({
-            text: input.text,
-            creator: new mongoose.mongo.ObjectID('6093e6a872228b2dc190b595')
-        })
-        const result = await content.save()
-        return {
-            message: 'Task created successfully', 
-            task: result
-        }
-    },
-    
-    putTask: async (args: IInputTask, req: Request) => {
-        const { input } = args
-        const result = await Task.findByIdAndUpdate(input.id, { text: input.text }, { new: true })
-        const tasks = await Task.find({})
-        return {
-            message: 'Task updated successfully',
-            task: result,
-            tasks: tasks
-        }
-    },
-
-    deleteTask: async (args: { id: string; }, req: Request) => {
-        const todoId = args.id
-        const tasks = await Task.findByIdAndRemove(todoId)
-        return {
-            message: 'Task deleted successfully'
-        }    
+    const content = new Task({
+      text: input.text,
+      creator: new mongoose.mongo.ObjectID('6093e6a872228b2dc190b595')
+    })
+    const result = await content.save()
+    return {
+      message: 'Task created successfully',
+      task: result
     }
-    
+  },
+
+  putTask: async (args: IInputTask, req: Request) => {
+    const { input } = args
+    const result = await Task.findByIdAndUpdate(input.id, { text: input.text }, { new: true })
+    const tasks = await Task.find({})
+    return {
+      message: 'Task updated successfully',
+      task: result,
+      tasks: tasks
+    }
+  },
+
+  deleteTask: async (args: { id: string }, req: Request) => {
+    const todoId = args.id
+    const tasks = await Task.findByIdAndRemove(todoId)
+    return {
+      message: 'Task deleted successfully'
+    }
+  }
+
 }
 
 export default graphqlResolvers
