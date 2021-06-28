53d9af10f3d458da5599beae8790b1e202e59238
Choor Hong <abner.hong@hotmail.com>
2021-06-28 22:42:58 +0800

    Edit cors configurations

--- backend/middlewares/cors.ts
+++ backend/middlewares/cors.ts
@@ -1,18 +1,18 @@
 import { Request, Response, NextFunction } from 'express'
 
 const cors = (req: Request, res: Response, next: NextFunction) => {
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    res.setHeader(
-      'Access-Control-Allow-Methods',
-      'OPTIONS, GET, POST, PUT, PATCH, DELETE'
-    );
-    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  res.setHeader('Access-Control-Allow-Origin', '*')
+  res.setHeader(
+    'Access-Control-Allow-Methods',
+    'OPTIONS, GET, POST, PUT, PATCH, DELETE'
+  )
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization')
 
-    if (req.method === 'OPTIONS') {
-      return res.sendStatus(200)
-    }
-    
-    next();
+  if (req.method === 'OPTIONS') {
+    return res.sendStatus(200)
+  }
+
+  next()
 }
 
-export default cors 
+export default cors
